90591ee622a4a3b61e91f7b88ead576a30abc592
kondo tomonori <tomonorik1001@gmail.com>
2021-05-14 15:35:21 +0900

    🎉

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '43406ee3eb72f6d1dc918f03d0c168ee');
